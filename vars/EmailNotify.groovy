90b51f1a43883c400c08338d6e9ab3c7e3bd1881
srujan6399 <srujanvarma30@gmail.com>
2017-08-10 20:58:38 -0500

    Create EmailNotify.groovy

---
+++ vars/EmailNotify.groovy
@@ -0,0 +1,22 @@
+#!/usr/bin/env groovy
+
+
+def call(body) {
+
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+  
+  def JENKINS_UID = config.status
+  def JENKINS_UID = config.attachment
+
+    String tolist="srujan.d87@gmail.com";
+    String sub="CI Jenkins ${status}: Job[${env.JOB_NAME}][${env.BUILD_NUMBER}]";
+    String body="<p>This Email is for Demo purpose only -${status}: Job'[${env.JOB_NAME}][${env.BUILD_NUMBER}]':</p>"+
+                "<p><b>Check console output at <a href='${env.BUILD_URL}'>${env.JOB_NAME}[${env.BUILD_NUMBER}]</a></b></p>";
+     
+    emailext body: body, subject: sub, to:tolist, mimeType: 'text/html', attachmentsPattern: attachment
+
+ 
+ }
