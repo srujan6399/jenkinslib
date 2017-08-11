0727bbf7f21a9f6c3710cc23a20b12eb7655458c
srujan6399 <srujanvarma30@gmail.com>
2017-08-10 21:13:01 -0500

    Update EmailNotify.groovy

--- vars/EmailNotify.groovy
+++ vars/EmailNotify.groovy
@@ -13,10 +13,10 @@ def call(body) {
 
     String tolist="srujan.d87@gmail.com";
     String sub="CI Jenkins ${status}: Job[${env.JOB_NAME}][${env.BUILD_NUMBER}]";
-    String body="<p>This Email is for Demo purpose only -${status}: Job'[${env.JOB_NAME}][${env.BUILD_NUMBER}]':</p>"+
+    String bdy="<p>This Email is for Demo purpose only -${status}: Job'[${env.JOB_NAME}][${env.BUILD_NUMBER}]':</p>"+
                 "<p><b>Check console output at <a href='${env.BUILD_URL}'>${env.JOB_NAME}[${env.BUILD_NUMBER}]</a></b></p>";
      
-    emailext body: body, subject: sub, to:tolist, mimeType: 'text/html', attachmentsPattern: attachment
+    emailext body: bdy, subject: sub, to:tolist, mimeType: 'text/html', attachmentsPattern: attachment
 
  
  }
